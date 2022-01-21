fa5c9f4c6cd354faa43fa489bc0283db0a6a22eb
ArneMayer <arnemayer94@gmail.com>
2022-01-21 12:50:21 +0100

    install -y in install script

--- install-docker.sh
+++ install-docker.sh
@@ -5,12 +5,12 @@ sudo apt-get update && sudo apt-get upgrade
 curl -sSL https://get.docker.com | sh
 
 # Install python
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt install -y python3-dev
 sudo apt-get install -y python3 python3-pip
 
 # Install python over pip
-sudo pip3 install docker-compose
+sudo pip3 install -y docker-compose
 
 # Start containers on boot
 sudo systemctl enable docker
