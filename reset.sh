acccb0e257c844d7bb91505691bc0072d654c7b6
Arne Mayer <arnemayer94@gmail.com>
2022-02-19 01:29:23 +0100

    fix install script for storage

--- reset.sh
+++ reset.sh
@@ -7,6 +7,7 @@ cd /docker
 rm -rf /docker/homeassistant/.storage
 rm -rf /mnt/ssd/mariadb_data
 rm -rf /mnt/ssd/mosquitto
+./install.sh
 docker-compose build
 docker-compose up -d --force-recreate
 systemctl start kioskapp
